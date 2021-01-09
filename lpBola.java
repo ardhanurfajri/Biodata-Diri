d30faa3297bf0f16ce8057d4ed61c0a393233dbd
ardhanurfajri <77182817+ardhanurfajri@users.noreply.github.com>
2021-01-09 11:49:33 +0700

    Add files via upload

---
+++ lpBola.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class lpBola {
+   public static void main(String[]args){
+       Scanner masukan=new Scanner(System.in);
+       System.out.println("=====================");
+       System.out.println("Program Konversi Suhu");
+       System.out.println("=====================");
+       float c=78,r,f,k;
+       r=4*c/5;
+       System.out.println(c+" Celcius = "+r+" Reamur");
+       f=9*c/5+32;
+       System.out.println(c+" Celcius = "+f+" Farenheit");
+       k=c+273;
+       System.out.println(c+" Celcius = "+k+" Kelvin");
+   } 
+}
